135cc4aea5db3666fe5add4b023c44b2178184f1
Aaron Ford <aaron@aaronford.net>
2018-07-18 02:05:01 +0000

    Added jquery form submission and validation

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,61 +4,11 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
- // Test / driver code (temporary). Eventually will get this from the server.
- const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "How did the hipster burn his tongue? He drank his coffee before it was cool"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "What did the mom coffee bean say to the kid coffee bean? You're grounded."
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Hey, wanna hear a joke about ghosts?<br/><br/> That's the spirit"
-    },
-    "created_at": 1461113796368
-  }
-];
-
-
 document.addEventListener('DOMContentLoaded', function(event) {
 
   function createTweetElement(data) {
-    let date = new Date(data.created_at * 1000);
+    let date = moment(data.created_at).fromNow();
     let $article = $('<article>').addClass('tweet');
-
     $article.html(`
       <header>
         <img src=${data.user.avatars.small} alt=${data.user.name}>
@@ -69,24 +19,66 @@ document.addEventListener('DOMContentLoaded', function(event) {
         <p>${data.content.text}</p>
       </div>
       <footer>
-        <p>${date.toLocaleDateString("en-US")}</p>
+        <p>${date}</p>
         <div class="footer-icons">
           <img src="https://png.icons8.com/windows/64/000000/flag.png" alt="flag">
           <img src="https://png.icons8.com/windows/64/000000/available-updates.png">
           <img src="https://png.icons8.com/windows/64/000000/hearts.png">
         </div>
       </footer>
-      `);
+    `);
     return $article;
   };
 
+  // RENDER ALL TWEETS IN DATABASE
   function renderTweets(data) {
     for (let tweets in data) {
-      let tweet = data[tweets];
-      let $tweet = createTweetElement(tweet);
+      let $tweet = createTweetElement(data[tweets]);
       $('#timeline').append($tweet);
     }
   };
 
-  renderTweets(data);
+
+  // POST NEW TWEET
+  $('.new-tweet form').on('submit', function(event){
+    event.preventDefault();
+    let $text = $(this).serialize();
+
+    if (validateForm($text)) {
+      $.ajax({
+        type: "POST",
+        url: '/tweets',
+        data: $text
+      });
+      $('.new-tweet textarea').val('');
+
+      // Clear timeline and load all tweets - needs to wait until BD is updated!
+      $('#timeline').empty();
+      loadTweets();
+    };
+  });
+
+  // VALIDATE FORM
+  function validateForm(input) {
+    let formText = input.split('=')[1];
+    if (formText.length <= 0) {
+      alert('You must enter a tweet to send');
+      return;
+    } else if (formText.length > 140 ) {
+      alert('Tweet too long - max 140 characters')
+      return;
+    }
+    return true;
+  }
+
+  //GET TWEETS
+  function loadTweets() {
+    $.ajax('/tweets', { method: 'GET' })
+    .then(function (tweets) {
+      //console.log('Success: ', tweets);
+      renderTweets(tweets);
+    });
+  }
+
+  loadTweets();
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
+
   $('.new-tweet textarea').on('input', function() {
     let count = 140 - this.value.length;
     let $counter = $(this).siblings(".counter");
     // Make counter text red if count goes below zero
     count >= 0 ? $counter.text(count).css('color', '#244751') : $counter.text(count).css('color', 'red');
-  })
+  });
+
 });
